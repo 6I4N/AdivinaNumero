45447586e0c1b4514ea08e66bc5dc4c4d1608306
giang <giang@GIAN>
2024-05-07 21:24:00 -0500

    Agregar archivos de proyecto.

---
+++ Practicas/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+
+namespace Practicas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int intentos = 1;
+            Random num = new Random();
+            int numAleatorio = num.Next(0,100);
+
+            Console.WriteLine("Adivine el número entre 0 a 100");
+            int respuesta = int.Parse(Console.ReadLine());
+
+            while (respuesta != numAleatorio)
+            {
+                intentos++;
+                
+                if(respuesta > numAleatorio) Console.WriteLine("El número es menor");
+
+                else Console.WriteLine("El número es mayor");
+
+                respuesta = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine($"Adivinaste el número|Número adivinado:{numAleatorio}|Intentos: {intentos}");
+        }
+    }
+}
